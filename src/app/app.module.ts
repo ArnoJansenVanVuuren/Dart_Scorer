2d5fe5989415dd36de0599ec870b6f3ed5615c65
WhiteThundra <87227088+WhiteThundra@users.noreply.github.com>
2022-04-14 22:16:27 +0200

    player page form start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import {
-  IonicModule,
-  IonicRouteStrategy,
-  MenuController,
-} from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,7 +22,12 @@ import { WelcomeComponent } from './pages/welcome/welcome.component';
     PlayersComponent,
   ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ReactiveFormsModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/pages/players/players.component.ts
+++ src/app/pages/players/players.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-players',
   templateUrl: './players.component.html',
   styleUrls: ['./players.component.scss'],
 })
-export class PlayersComponent implements OnInit {
+export class PlayersComponent implements OnInit, OnDestroy {
+  playerGameForm: FormGroup;
+  playerArr: string[] = [];
+  killSub: Subscription;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.playerGameForm = this.fb.group({
+      playerQty: [null, Validators.required],
+      killerVal: [null, Validators.required],
+    });
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    //---subscribe to player QTY changes
+    this.killSub =
+      this.playerGameForm.controls.playerQty.valueChanges.subscribe((val) => {
+        //---clear player array
+        this.playerArr = [];
+        //---add '' for QTY of players
+        for (let i = 0; i < val; i++) {
+          this.playerArr.push('' + i);
+        }
+        console.log(this.playerArr);
+      });
+  }
 
+  ngOnDestroy(): void {
+    this.killSub.unsubscribe();
+  }
 }
