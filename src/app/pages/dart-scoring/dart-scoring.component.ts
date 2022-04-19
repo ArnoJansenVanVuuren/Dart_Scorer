6210d3c62e05d29a4a7bb0b3b057e1983c1f2eb9
Jakes <jakesfcoetz@gmail.com>
2022-04-19 14:30:59 +0200

    dart scoring init

---
+++ src/app/pages/dart-scoring/dart-scoring.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dart-scoring',
+  templateUrl: './dart-scoring.component.html',
+  styleUrls: ['./dart-scoring.component.scss'],
+})
+export class DartScoringComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
