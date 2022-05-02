70a1acd5d09f9a4932bafb9399663ed4ba94d457
WhiteThundra <arno24589@gmail.com>
2022-05-02 15:07:47 +0200

    dart scoring updated with single double tripple

--- src/app/components/dart-scoring/dart-scoring.component.ts
+++ src/app/components/dart-scoring/dart-scoring.component.ts
@@ -8,25 +8,33 @@ import { ModalController } from '@ionic/angular';
 })
 export class DartScoringComponent implements OnInit {
   scoringValues: { id: number; name: string | number; value: number }[] = [];
-
+  selectedValue: any = 0;
   constructor(private modalController: ModalController) {}
 
   //--make sure array dart numbers are populated if not then populate
   ionViewWillEnter() {
     if (this.scoringValues.length === 0) {
-      for (let i = 0; i < 21; i++) {
+      this.scoringValues.push({ id: 0, name: 'miss', value: 0 });
+
+      for (let i = 1; i < 21; i++) {
         this.scoringValues.push({ id: i, name: i, value: i });
       }
       this.scoringValues.push(
         { id: 21, name: 'bull', value: 25 },
         { id: 22, name: 'cherry', value: 50 }
       );
+      console.log(this.scoringValues);
     }
   }
 
   ngOnInit() {}
 
+  valueSelected(value) {
+    this.selectedValue = value;
+    console.log('selected', this.selectedValue);
+  }
   dismiss(value) {
-    this.modalController.dismiss(value);
+    this.selectedValue = this.selectedValue * value;
+    this.modalController.dismiss(this.selectedValue);
   }
 }
